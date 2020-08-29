d628aead21cfed89fcbf011c910f3126b89f1424
kazeem <kazeem0825@gmail.com>
2020-08-29 22:50:37 +0100

    add controller, fix import error

--- controllers/car.go
+++ controllers/car.go
@@ -1,10 +1,13 @@
-package main
+package controllers
 
 import (
 	"../models"
 	"encoding/json"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"net/http"
 )
 
@@ -12,14 +15,35 @@ type CarController struct {
 	session *mgo.Session
 }
 
+var collection = "cars"
+var dbName = "automobiles"
+
 func NewCarController(s *mgo.Session) *CarController {
 	return &CarController{s}
 }
 
 func (cc CarController) CreateCar(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-	c := models.Car{}
+	car := models.Car{}
 
 	// decode json
-	json.NewDecoder(r.Body).Decode(&c)
+	json.NewDecoder(r.Body).Decode(&car)
+
+	// generate an ID
+	car.Id = bson.NewObjectId()
+
+	// store car in mongo db
+	cc.session.DB(dbName).C(collection).Insert(car)
+
+	// convert car to json
+	c, err := json.Marshal(car)
+
+	// handle error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println(c)
+
 
 }
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"./controllers"
 	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 	"net/http"
 )
 
 
-
-
 func main() {
 	r := httprouter.New()
-	r.GET("/", GetUsers)
+	carController := controllers.NewCarController(getSession())
+	r.GET("/", carController.CreateCar)
 
 	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	// connect to local mongo
+	s, err := mgo.Dial("localhost")
+
+	// check if connection error, is mongo running
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
 
